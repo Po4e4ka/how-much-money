8c45fc03669d857d74ed55d8b4e1f7386e5d50e1
Алексей Михайлов <okayfantasy@gmail.com>
2026-02-04 12:31:29 +0300

    version: 0.1.2
    - исправления UI на странице с периодами при создании нового периода
    - исправлено это окошко
    - исправления UI страницы периода
    - добавлен новый блок "рассчёт среднего на оставшиеся дни"
    - поправлена цифра "Фактически среднее за период" - теперь считаются только заполненные дни

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,13 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'periods' => [
+            'driver' => 'single',
+            'path' => base_path('var/logs/laravel/periods.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
+        ],
+
     ],
 
 ];
